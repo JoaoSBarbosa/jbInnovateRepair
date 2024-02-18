52f172ba376a90e457567761589c2bca6e2c4e15
João Barbosa <contato.jsbarbosa@gmail.com>
2024-02-18 13:11:58 -0300

    Tela principal - Implementado menus, logo, e campos iniciais de exibição

--- src/br/com/jbinnovateRepair/telas/TelaPrincipal.java
+++ src/br/com/jbinnovateRepair/telas/TelaPrincipal.java
@@ -28,34 +28,145 @@ public class TelaPrincipal extends javax.swing.JFrame {
     private void initComponents() {
 
         jCheckBoxMenuItem1 = new javax.swing.JCheckBoxMenuItem();
-        jLabel1 = new javax.swing.JLabel();
+        DesktopLayout = new javax.swing.JDesktopPane();
+        logo = new javax.swing.JLabel();
+        loggedInUser = new javax.swing.JLabel();
+        currentDate = new javax.swing.JLabel();
+        MenuBar = new javax.swing.JMenuBar();
+        menCad = new javax.swing.JMenu();
+        menCadClient = new javax.swing.JMenuItem();
+        menCadOs = new javax.swing.JMenuItem();
+        menCadUser = new javax.swing.JMenuItem();
+        menReport = new javax.swing.JMenu();
+        menReportService = new javax.swing.JMenuItem();
+        menHelp = new javax.swing.JMenu();
+        menHelpAbout = new javax.swing.JMenuItem();
+        menOptions = new javax.swing.JMenu();
+        menOptionsExit = new javax.swing.JMenuItem();
 
         jCheckBoxMenuItem1.setSelected(true);
         jCheckBoxMenuItem1.setText("jCheckBoxMenuItem1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("JB Innovate Repair - Sistema de gerenciamento de Ordens de Serviços");
+        setResizable(false);
 
-        jLabel1.setText("TELA PRINCIPAL");
+        DesktopLayout.setPreferredSize(new java.awt.Dimension(640, 480));
+
+        javax.swing.GroupLayout DesktopLayoutLayout = new javax.swing.GroupLayout(DesktopLayout);
+        DesktopLayout.setLayout(DesktopLayoutLayout);
+        DesktopLayoutLayout.setHorizontalGroup(
+            DesktopLayoutLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 836, Short.MAX_VALUE)
+        );
+        DesktopLayoutLayout.setVerticalGroup(
+            DesktopLayoutLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
+        logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/jbinnovateRepair/icones/logo3.png"))); // NOI18N
+        logo.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        logo.setIconTextGap(0);
+        logo.setMaximumSize(new java.awt.Dimension(200, 200));
+        logo.setMinimumSize(new java.awt.Dimension(200, 200));
+        logo.setPreferredSize(new java.awt.Dimension(200, 200));
+
+        loggedInUser.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        loggedInUser.setText("Usuário");
+
+        currentDate.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
+        currentDate.setText("Data");
+
+        menCad.setText("Cadastro");
+
+        menCadClient.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_C, java.awt.event.InputEvent.ALT_MASK));
+        menCadClient.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/jbinnovateRepair/icones/clientes_24.png"))); // NOI18N
+        menCadClient.setText("Cliente");
+        menCad.add(menCadClient);
+
+        menCadOs.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_O, java.awt.event.InputEvent.ALT_MASK));
+        menCadOs.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/jbinnovateRepair/icones/os_24.png"))); // NOI18N
+        menCadOs.setText("OS");
+        menCad.add(menCadOs);
+
+        menCadUser.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_U, java.awt.event.InputEvent.ALT_MASK));
+        menCadUser.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/jbinnovateRepair/icones/suporte_24.png"))); // NOI18N
+        menCadUser.setText("Usuários");
+        menCad.add(menCadUser);
+
+        MenuBar.add(menCad);
+
+        menReport.setText("Relatório");
+
+        menReportService.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.event.InputEvent.ALT_MASK));
+        menReportService.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/jbinnovateRepair/icones/relatorio.png"))); // NOI18N
+        menReportService.setText("Serviços");
+        menReportService.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menReportServiceActionPerformed(evt);
+            }
+        });
+        menReport.add(menReportService);
+
+        MenuBar.add(menReport);
+
+        menHelp.setText("Ajuda");
+
+        menHelpAbout.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F1, java.awt.event.InputEvent.ALT_MASK));
+        menHelpAbout.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/jbinnovateRepair/icones/sobre.png"))); // NOI18N
+        menHelpAbout.setText("Sobre");
+        menHelp.add(menHelpAbout);
+
+        MenuBar.add(menHelp);
+
+        menOptions.setText("Opções");
+
+        menOptionsExit.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F4, java.awt.event.InputEvent.ALT_MASK));
+        menOptionsExit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/jbinnovateRepair/icones/sair.png"))); // NOI18N
+        menOptionsExit.setText("Sair");
+        menOptions.add(menOptionsExit);
+
+        MenuBar.add(menOptions);
+
+        setJMenuBar(MenuBar);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(301, 301, 301)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 387, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(267, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(DesktopLayout, javax.swing.GroupLayout.DEFAULT_SIZE, 836, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(currentDate)
+                    .addComponent(loggedInUser)
+                    .addComponent(logo, javax.swing.GroupLayout.DEFAULT_SIZE, 241, Short.MAX_VALUE))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
-                .addContainerGap(447, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(loggedInUser)
+                        .addGap(18, 18, 18)
+                        .addComponent(currentDate)
+                        .addGap(142, 142, 142)
+                        .addComponent(logo, javax.swing.GroupLayout.DEFAULT_SIZE, 210, Short.MAX_VALUE)
+                        .addGap(0, 185, Short.MAX_VALUE))
+                    .addComponent(DesktopLayout, javax.swing.GroupLayout.DEFAULT_SIZE, 606, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
-        pack();
+        setSize(new java.awt.Dimension(1123, 688));
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void menReportServiceActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menReportServiceActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_menReportServiceActionPerformed
 
     /**
      * @param args the command line arguments
@@ -93,7 +204,21 @@ public class TelaPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JDesktopPane DesktopLayout;
+    private javax.swing.JMenuBar MenuBar;
+    private javax.swing.JLabel currentDate;
     private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem1;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel loggedInUser;
+    private javax.swing.JLabel logo;
+    private javax.swing.JMenu menCad;
+    private javax.swing.JMenuItem menCadClient;
+    private javax.swing.JMenuItem menCadOs;
+    private javax.swing.JMenuItem menCadUser;
+    private javax.swing.JMenu menHelp;
+    private javax.swing.JMenuItem menHelpAbout;
+    private javax.swing.JMenu menOptions;
+    private javax.swing.JMenuItem menOptionsExit;
+    private javax.swing.JMenu menReport;
+    private javax.swing.JMenuItem menReportService;
     // End of variables declaration//GEN-END:variables
 }
